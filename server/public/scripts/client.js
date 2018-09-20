98cd6a8e29388c8020593532adee09e7fbf026a7
Christopher Brenberg <cbrenberg@gmail.com>
2018-09-19 22:39:00 -0500

    POST working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,9 +3,37 @@ const shoeApp = angular.module('ShoeApp', []);
 shoeApp.controller('ShoeController', ['$http', function($http) {
   const vm = this;
   
-vm.message = 'Angular loaded!';
+  vm.message = 'Angular loaded!';
+  vm.shoeType = '';
+  vm.price = '';
 
   vm.addNewShoe = function () {
     console.log('in addNewShoe');
-  }
+    $http({
+      method: 'POST',
+      url: '/shoes',
+      data: {
+        "name": vm.shoeType,
+        "cost": vm.price
+      }
+    }).then((response) => {
+      console.log('Back from /POST:', response);
+    }).catch((error) => {
+      console.log('Error adding shoes:', error);
+    })
+  };
+
+  // vm.getShoes = function () {
+  //   console.log('in getShoes');
+  //   $http({
+  //     method: 'POST',
+  //     url: '/shoes'
+  //     params: {
+        
+  //     }
+  //   })
+
+  // }
+
+
 }]);
